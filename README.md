8da0dfe9cee280258c140cdaa755d055efa4996b
Frankie Foo <Foo_XianYang@jabil.com>
2019-03-27 21:30:12 +0800

    Initial upload.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Bumptech.Glide.Integration.RecyclerView
+
+![logo](https://raw.githubusercontent.com/xyfoo/Bumptech.Glide.Integration.RecyclerView/master/art/logo.png)
+
 Xamarin.Android implementation of Glide's RecyclerView integration library
+
+[![NuGet](https://img.shields.io/nuget/vpre/Bumptech.Glide.Integration.RecyclerView.svg?label=NuGet)](https://www.nuget.org/packages/Bumptech.Glide.Integration.RecyclerView)
+
+## Introduction
+
+This is a C# implementation of Glide's RecycleView intergration library.
+
+
+>[Glide](https://bumptech.github.io/glide/) is a fast and efficient image loading library for Android focused on smooth scrolling. Glide offers an easy to use API, a performant and extensible resource decoding pipeline and automatic resource pooling.
+>
+> ...
+>
+> The [RecyclerView integration library](https://bumptech.github.io/glide/int/recyclerview.html) makes the RecyclerViewPreloader available in your application. RecyclerViewPreloader can automatically load images just ahead of where a user is scrolling in a RecyclerView.
+>
+>Combined with the right image size and an effective disk cache strategy, this library can dramatically decrease the number of loading tiles/indicators users see when scrolling through lists of images by ensuring that the images the user is about to reach are already in memory.
+
+## Dependencies
+
+* Xamarin.Android.Glide, v 4.0.0
+
+## Walkthrough
+
+TODO
+
+---
+
+## FAQ
+
+### Why reimplement instead of creating a binding library?
+
+Attempts to create binding to the [4.0.0 library](https://github.com/bumptech/glide/releases/download/v4.0.0/glide-recyclerview-integration-4.0.0.jar) yield this error: ```BINDINGSGENERATOR : warning BG8601: No packages found```... and I have no idea why that happened.
+
+It turns out the sources code is small, and a reimplementation in C# is faster than to figure out why the bindings build failed.
+
+### The Glide version used by Xamarin.Android.Glide is outdated (v4.0.0)
+
+Xamarin.Android.Glide v4.0.0 package is the only one nuget package with full blown bindings. It's probably easier to take a Xamarin official bindings instead of taking another unproven one.
+
+[Discussion on updating Xamarin.Android.Glide](https://github.com/xamarin/XamarinComponents/issues/464)
+
+### Can this works on later version of Glide?
+
+I didn't notice any difference between v4.0-v4.8 (latest checked on 27 Match 2019) of this RecyclerView integration library. 
+
+So, if you can find a newer binding library for Glide, you should be able copy/paste the content in ```RecyclerView.cs``` and it should work.
---
+++ source/RecyclerView.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+using Android.App;
+using Android.Support.V4.App;
+using Android.Support.V7.Widget;
+using Android.Widget;
+using static Android.Support.V7.Widget.RecyclerView;
+using static Android.Widget.AbsListView;
+using static Bumptech.Glide.ListPreloader;
+
+namespace Bumptech.Glide.Integration.RecyclerView
+{
+    /// <summary>
+    /// Converts android.support.v7.widget.RecyclerView.OnScrollListener events to AbsListView scroll events.
+    /// 
+    /// Requires that the the recycler view be using a LinearLayoutManager subclass.
+    /// </summary>
+    public class RecyclerToListViewScrollListener : OnScrollListener
+    {
+        private readonly IOnScrollListener _scrollListener;
+        private int _lastFirstVisible = -1;
+        private int _lastVisibleCount = -1;
+        private int _lastItemCount = -1;
+
+        public RecyclerToListViewScrollListener(IOnScrollListener scrollListener)
+        {
+            _scrollListener = scrollListener;
+        }
+
+        public override void OnScrollStateChanged(Android.Support.V7.Widget.RecyclerView recyclerView, int newState)
+        {
+            ScrollState listViewState = ScrollState.Idle;
+            switch (newState)
+            {
+                case ScrollStateDragging:
+                    listViewState = ScrollState.TouchScroll;
+                    break;
+                case ScrollStateIdle:
+                    listViewState = ScrollState.Idle;
+                    break;
+                case ScrollStateSettling:
+                    listViewState = ScrollState.Fling;
+                    break;
+            }
+
+            _scrollListener.OnScrollStateChanged(null, listViewState);
+        }
+
+        public override void OnScrolled(Android.Support.V7.Widget.RecyclerView recyclerView, int dx, int dy)
+        {
+            LinearLayoutManager layoutManager = (LinearLayoutManager)recyclerView.GetLayoutManager();
+
+            int firstVisible = layoutManager.FindFirstVisibleItemPosition();
+            int visibleCount = Math.Abs(firstVisible - layoutManager.FindLastCompletelyVisibleItemPosition());
+            int itemCount = recyclerView.GetAdapter().ItemCount;
+
+            if (firstVisible != _lastFirstVisible || visibleCount != _lastVisibleCount || itemCount != _lastItemCount)
+            {
+                _scrollListener.OnScroll(null, firstVisible, visibleCount, itemCount);
+                _lastVisibleCount = firstVisible;
+                _lastVisibleCount = visibleCount;
+                _lastItemCount = itemCount;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Loads a few resources ahead in the direction of scrolling in any RecyclerView so that
+    /// images are in the memory cache just before the corresponding view in created in the list. Gives
+    /// the appearance of an infinitely large image cache, depending on scrolling speed, cpu speed, and
+    /// cache size.
+    /// 
+    /// Must be added as a listener to the RecycleView using RecyclerView.AddOnScrollListener(...),
+    /// or have it's corresponding methods called from another RecyclerView.OnScrollListener to function.
+    /// 
+    /// This class only works with android.support.v7.widget.LinearLayoutManager and subclasses of android.support.v7.widget.LinearLayoutManager.
+    /// </summary>
+    /// <typeparam name="T">The type of the model being displayed in the RecyclerView</typeparam>
+    public class RecyclerViewPreloader<T> : OnScrollListener
+    {
+        private readonly RecyclerToListViewScrollListener _recyclerScrollListener;
+
+        /// <summary>
+        /// Constructor that accepts interfaces for providing the dimensions of images to preload, the list
+        /// of models to preload for a given position, and the request to use to load images.
+        /// </summary>
+        /// <param name="requestManager"></param>
+        /// <param name="preloadModelProvider">Provides models to load and requests capable of loading them.</param>
+        /// <param name="preloadDimensionProvider">Provides the dimensions of images to load.</param>
+        /// <param name="maxPreload">Maximum number of items to preload.</param>
+        public RecyclerViewPreloader(RequestManager requestManager, IPreloadModelProvider preloadModelProvider, IPreloadSizeProvider preloadDimensionProvider, int maxPreload)
+        {
+            ListPreloader listPreloader = new ListPreloader(requestManager, preloadModelProvider, preloadDimensionProvider, maxPreload);
+            _recyclerScrollListener = new RecyclerToListViewScrollListener(listPreloader);
+        }
+
+        public override void OnScrolled(Android.Support.V7.Widget.RecyclerView recyclerView, int dx, int dy)
+        {
+            _recyclerScrollListener.OnScrolled(recyclerView, dx, dy);
+        }
+
+        /// <summary>
+        /// Helper constructor that accepts an Activity.
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <param name="preloadModelProvider"></param>
+        /// <param name="preloadDimensionProvider"></param>
+        /// <param name="maxPreload"></param>
+        public RecyclerViewPreloader(Activity activity, IPreloadModelProvider preloadModelProvider, IPreloadSizeProvider preloadDimensionProvider, int maxPreload)
+            : this(Glide.With(activity), preloadModelProvider, preloadDimensionProvider, maxPreload)
+        {
+        }
+
+        /// <summary>
+        /// Helper constructor that accepts an FragmentActivity.
+        /// </summary>
+        /// <param name="fragmentActivity"></param>
+        /// <param name="preloadModelProvider"></param>
+        /// <param name="preloadDimensionProvider"></param>
+        /// <param name="maxPreload"></param>
+        public RecyclerViewPreloader(FragmentActivity fragmentActivity, IPreloadModelProvider preloadModelProvider, IPreloadSizeProvider preloadDimensionProvider, int maxPreload)
+            : this(Glide.With(fragmentActivity), preloadModelProvider, preloadDimensionProvider, maxPreload)
+        {
+        }
+
+        /// <summary>
+        /// Helper constructor that accepts an Fragment.
+        /// </summary>
+        /// <param name="fragment"></param>
+        /// <param name="preloadModelProvider"></param>
+        /// <param name="preloadDimensionProvider"></param>
+        /// <param name="maxPreload"></param>
+        public RecyclerViewPreloader(Android.App.Fragment fragment, IPreloadModelProvider preloadModelProvider, IPreloadSizeProvider preloadDimensionProvider, int maxPreload)
+            : this(Glide.With(fragment), preloadModelProvider, preloadDimensionProvider, maxPreload)
+        {
+        }
+    }
+}
